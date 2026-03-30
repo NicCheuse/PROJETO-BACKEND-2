ca57011ddfaac8bbb9f536753ab4912576fbd75d
Nic <ramos.nicollas@escola.pr.gov.br>
2026-03-30 11:01:15 +0000

    top

---
+++ funções/fuc-02.js
@@ -0,0 +1,9 @@
+
+function subtracao(n1, n2) {
+    const resultado = n1 - n2;
+    console.log("Resualtado da subtração: " + resultado);
+
+}
+
+subtracao(5, 2);
+subtracao(-50, 10);
---
+++ funções/func-01.js
@@ -0,0 +1,10 @@
+function dizOla() {
+    console.log('Olá');
+}
+function olaPessoa(nome) {
+    console.log('ola, ' + nome);
+}
+
+dizOla(); 
+olaPessoa("Luana");
+olaPessoa("Mano");
