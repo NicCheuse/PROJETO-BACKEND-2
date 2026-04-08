5f7928ac037e88eb0781ce2fae3a01519a3803fd
Nic <ramos.nicollas@escola.pr.gov.br>
2026-04-08 12:33:57 +0000

    bacanas

--- funções/funcao-03.js
+++ funções/funcao-03.js
@@ -2,5 +2,7 @@ function Sobradosalario(meuSalario){
     const contadeluz = 200;
     const aluguel = 1000;
 const resto = meuSalario - (contadeluz + aluguel);
-console.log(`O valor que sobra do meu salário é: ${resto}`);
+return `O valor que sobra do meu salário é: ${resto}`
 }
+const minhasSobras = Sobradosalario(7500);
+console.log(minhasSobras);
