36466c104952ff3932dac2a9c7afcc1df2dbbe65
Nic <ramos.nicollas@escola.pr.gov.br>
2026-06-10 11:44:43 +0000

    arquivos de leitura bacanos

---
+++ 2º TRIMESTRE/modulos/mod03-fs/fs-chalk.js
@@ -0,0 +1,16 @@
+const {default: chalk} = require('chalk')
+const fs = require('fs')
+const path = require('path')    
+const chalk = require ('chalk').default
+try {
+    const caminho = path.resolve(__dirname, 'README.md')
+    const arquivo = fs.readFileSync(caminho, 'utf-8')
+    console.log(chalk.green('arquivo carregado com sucesso!'))
+
+
+    console.log(arquivo)
+} catch (error) {
+    console.log(chalk.yellow('falha ao carregar o arquivo. Tente novamente mais tarde'))
+    console.log(chalk.red('Erro interno de leitura de arquivo: \n', error.message))
+}
+    
