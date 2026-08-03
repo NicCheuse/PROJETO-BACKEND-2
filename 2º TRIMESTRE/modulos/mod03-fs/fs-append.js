9dcc61292b5cda0e1cab0be59bc74892c6aaee4c
Nic <ramos.nicollas@escola.pr.gov.br>
2026-08-03 11:00:51 +0000

    coisas boas ai que perdi

--- 2º TRIMESTRE/modulos/mod03-fs/fs-append.js
+++ 2º TRIMESTRE/modulos/mod03-fs/fs-append.js
@@ -1,14 +1,17 @@
 
 const fs = require('fs')
 const path = require('path')
-const frasePreferida = `Avante Renan Santos e sucumba Lula e PT`
-const log = `Atenção, você gosta de coisas baitolas`
+const frasePreferida = `Avante Renan Santos e sucumba Lula e PT!\n`
+const log = `Atenção, você gosta de coisas baitolas...\n`
 
 try{
 
 const arquivo = path.resolve(__dirname, 'frase.md')
-fs.writeFileSync(arquivo, frasePreferida, 'utf-8')
+fs.appendFileSync(arquivo, frasePreferida, 'utf-8')
 
+//Reescreve totalmente o conteúdo
+fs.appendFileSync(arquivo, log, 'utf-8')
+fs.appendFileSync(arquivo, log, 'utf-8')
 }catch (error) {
     console.log('Erro ao criar o arquivo: \n', error.message)
 
